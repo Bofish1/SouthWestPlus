28bbaad9211614ae85281a6d6d38c4a15ec2f415
unknown <Bofish@gmail.com>
2023-02-19 20:35:43 -0600

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SouthWestPlus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
